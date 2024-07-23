4bff52e83294c3b1b4cf21e71fdc0f46744796c9
Aldo <aldonicholas11@gmail.com>
2024-07-23 12:31:29 +0700

    feat: hide sidebar when not logged in

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -19,6 +19,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
   let storedSidebarExpanded = "true";
 
+  const user = localStorage.getItem("user");
+
   const [sidebarExpanded, setSidebarExpanded] = useState(
     storedSidebarExpanded === null ? false : storedSidebarExpanded === "true",
   );
@@ -61,67 +63,69 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   }, [sidebarExpanded]);
 
   return (
-    <aside
-      ref={sidebar}
-      className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
-        sidebarOpen ? "translate-x-0" : "-translate-x-full"
-      }`}
-    >
-      {/* <!-- SIDEBAR HEADER --> */}
-      <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-        <Link href="/">
-          <Image
-            width={176}
-            height={32}
-            src={"/images/logo/logo.svg"}
-            alt="Logo"
-            priority
-          />
-        </Link>
-
-        <button
-          ref={trigger}
-          onClick={() => setSidebarOpen(!sidebarOpen)}
-          aria-controls="sidebar"
-          aria-expanded={sidebarOpen}
-          className="block lg:hidden"
+    <>
+      {user && (
+        <aside
+          ref={sidebar}
+          className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
+            sidebarOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
         >
-          <svg
-            className="fill-current"
-            width="20"
-            height="18"
-            viewBox="0 0 20 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
-              fill=""
-            />
-          </svg>
-        </button>
-      </div>
-      {/* <!-- SIDEBAR HEADER --> */}
+          {/* <!-- SIDEBAR HEADER --> */}
+          <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
+            <Link href="/">
+              <Image
+                width={176}
+                height={32}
+                src={"/images/logo/logo.svg"}
+                alt="Logo"
+                priority
+              />
+            </Link>
 
-      <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
-        {/* <!-- Sidebar Menu --> */}
-        <nav className="mt-5 px-4 py-4 lg:mt-9 lg:px-6">
-          {/* <!-- Menu Group --> */}
-          <div>
-            <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
-              MENU
-            </h3>
+            <button
+              ref={trigger}
+              onClick={() => setSidebarOpen(!sidebarOpen)}
+              aria-controls="sidebar"
+              aria-expanded={sidebarOpen}
+              className="block lg:hidden"
+            >
+              <svg
+                className="fill-current"
+                width="20"
+                height="18"
+                viewBox="0 0 20 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
+                  fill=""
+                />
+              </svg>
+            </button>
+          </div>
+          {/* <!-- SIDEBAR HEADER --> */}
 
-            <ul className="mb-6 flex flex-col gap-1.5">
-              {/* <!-- Menu Item Dashboard --> */}
-              <li>
-                <Link
-                  href="/"
-                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname === "/" && "bg-graydark dark:bg-meta-4"
-                  }`}
-                >
-                  {/* <svg
+          <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
+            {/* <!-- Sidebar Menu --> */}
+            <nav className="mt-5 px-4 py-4 lg:mt-9 lg:px-6">
+              {/* <!-- Menu Group --> */}
+              <div>
+                <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
+                  MENU
+                </h3>
+
+                <ul className="mb-6 flex flex-col gap-1.5">
+                  {/* <!-- Menu Item Dashboard --> */}
+                  <li>
+                    <Link
+                      href="/"
+                      className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                        pathname === "/" && "bg-graydark dark:bg-meta-4"
+                      }`}
+                    >
+                      {/* <svg
                     className="fill-current"
                     width="18"
                     height="18"
@@ -134,99 +138,102 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                       fill=""
                     />
                   </svg> */}
-                  Dashboard
-                </Link>
-              </li>
-              {/* <!-- Menu Item Dashboard --> */}
+                      Dashboard
+                    </Link>
+                  </li>
+                  {/* <!-- Menu Item Dashboard --> */}
 
-              {/* <!-- Menu Item Kategori --> */}
-              <li>
-                <Link
-                  href="/kategori"
-                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("kategori") &&
-                    "bg-graydark dark:bg-meta-4"
-                  }`}
-                >
-                  Kategori
-                </Link>
-              </li>
-              {/* <!-- Menu Item Kategori --> */}
+                  {/* <!-- Menu Item Kategori --> */}
+                  <li>
+                    <Link
+                      href="/kategori"
+                      className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                        pathname.includes("kategori") &&
+                        "bg-graydark dark:bg-meta-4"
+                      }`}
+                    >
+                      Kategori
+                    </Link>
+                  </li>
+                  {/* <!-- Menu Item Kategori --> */}
 
-              {/* <!-- Menu Item Data Barang --> */}
-              <li>
-                <Link
-                  href="/data-barang"
-                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("data-barang") &&
-                    "bg-graydark dark:bg-meta-4"
-                  }`}
-                >
-                  Data Barang
-                </Link>
-              </li>
-              {/* <!-- Menu Item Data Barang --> */}
+                  {/* <!-- Menu Item Data Barang --> */}
+                  <li>
+                    <Link
+                      href="/data-barang"
+                      className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                        pathname.includes("data-barang") &&
+                        "bg-graydark dark:bg-meta-4"
+                      }`}
+                    >
+                      Data Barang
+                    </Link>
+                  </li>
+                  {/* <!-- Menu Item Data Barang --> */}
 
-              {/* <!-- Menu Item Tambah Barang --> */}
-              <li>
-                <Link
-                  href="/tambah-barang"
-                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("tambah-barang") &&
-                    "bg-graydark dark:bg-meta-4"
-                  }`}
-                >
-                  Tambah Barang
-                </Link>
-              </li>
-              {/* <!-- Menu Item Tambah Barang --> */}
+                  {/* <!-- Menu Item Tambah Barang --> */}
+                  <li>
+                    <Link
+                      href="/tambah-barang"
+                      className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                        pathname.includes("tambah-barang") &&
+                        "bg-graydark dark:bg-meta-4"
+                      }`}
+                    >
+                      Tambah Barang
+                    </Link>
+                  </li>
+                  {/* <!-- Menu Item Tambah Barang --> */}
 
-              {/* <!-- Menu Item Penjualan --> */}
-              <li>
-                <Link
-                  href="/penjualan"
-                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("penjualan") &&
-                    "bg-graydark dark:bg-meta-4"
-                  }`}
-                >
-                  Penjualan
-                </Link>
-              </li>
-              {/* <!-- Menu Item Penjualan --> */}
+                  {/* <!-- Menu Item Penjualan --> */}
+                  <li>
+                    <Link
+                      href="/penjualan"
+                      className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                        pathname.includes("penjualan") &&
+                        "bg-graydark dark:bg-meta-4"
+                      }`}
+                    >
+                      Penjualan
+                    </Link>
+                  </li>
+                  {/* <!-- Menu Item Penjualan --> */}
 
-              {/* <!-- Menu Item Laporan --> */}
-              <li>
-                <Link
-                  href="/laporan"
-                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("laporan") && "bg-graydark dark:bg-meta-4"
-                  }`}
-                >
-                  Laporan
-                </Link>
-              </li>
-              {/* <!-- Menu Item Laporan --> */}
+                  {/* <!-- Menu Item Laporan --> */}
+                  <li>
+                    <Link
+                      href="/laporan"
+                      className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                        pathname.includes("laporan") &&
+                        "bg-graydark dark:bg-meta-4"
+                      }`}
+                    >
+                      Laporan
+                    </Link>
+                  </li>
+                  {/* <!-- Menu Item Laporan --> */}
 
-              {/* <!-- Menu Item Prediksi --> */}
-              <li>
-                <Link
-                  href="/prediksi"
-                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("prediksi") &&
-                    "bg-graydark dark:bg-meta-4"
-                  }`}
-                >
-                  Prediksi
-                </Link>
-              </li>
-              {/* <!-- Menu Item Prediksi --> */}
-            </ul>
+                  {/* <!-- Menu Item Prediksi --> */}
+                  <li>
+                    <Link
+                      href="/prediksi"
+                      className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                        pathname.includes("prediksi") &&
+                        "bg-graydark dark:bg-meta-4"
+                      }`}
+                    >
+                      Prediksi
+                    </Link>
+                  </li>
+                  {/* <!-- Menu Item Prediksi --> */}
+                </ul>
+              </div>
+            </nav>
+            {/* <!-- Sidebar Menu --> */}
           </div>
-        </nav>
-        {/* <!-- Sidebar Menu --> */}
-      </div>
-    </aside>
+        </aside>
+      )}
+    </>
   );
 };
 
